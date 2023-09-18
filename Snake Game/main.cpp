5345d06ceaa7d21d670b51e4f924c85e55ad56e9
Alexandr <sashatataru2003@gmail.com>
2023-09-18 14:05:14 +0300

    Initial Commit

---
+++ Snake Game/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Snake Game
+//
+//  Created by Саша Татару on 18.09.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
